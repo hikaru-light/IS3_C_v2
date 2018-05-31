469414d2c6e8eba37e25513fe11821eed3ec5ae0
Hikaru Suido <hikaru.suido@gmail.com>
2018-05-31 14:44:42 +0900

    Update

--- search.c
+++ search.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-#define HSIZE 17
+#define HSIZE 997
 
 struct cell {
 	char *pcode;
@@ -14,18 +15,49 @@ struct cell {
 struct cell *p_hstab[HSIZE];
 struct cell *a_hstab[HSIZE];
 
-int p_hash(char *pcode) {
-    	int num = atoi(pcode) % HSIZE;
-	return num;
+int sum(char *x) {
+	int sum = 0;
+	while(*x) {
+		sum += (int)(*x);
+		x++;
+	}
+
+	return sum;
 }
 
-//int a_hash(char *addr) {
-//
-//}
+int p_hash(char *pcode) {
+    	return atoi(pcode) % HSIZE;
+}
 
-//void search(char *x) {
-//
-//}
+int a_hash(char *addr) {
+	return -(sum(addr) % HSIZE);
+}
+
+void p_search(struct cell *p_hstab, char *x) {
+	if(p_hstab == NULL) {
+		printf("No Data\n");
+		return;
+	}
+
+	if(strcmp(p_hstab->pcode, x) == 0){
+		printf("%s\n", p_hstab->addr);
+	} else {
+		p_search(p_hstab->p_next, x);
+	}
+}
+
+void a_search(struct cell *a_hstab, char *x) {
+	if(a_hstab == NULL) {
+		printf("No Data\n");
+		return;
+	}
+
+	if(strcmp(a_hstab->addr, x) == 0){
+		printf("%s\n", a_hstab->pcode);
+	} else {
+		a_search(a_hstab->a_next, x);
+	}
+}
 
 void show(struct cell *hstab){
 	if(hstab == NULL) {
@@ -34,23 +66,35 @@ void show(struct cell *hstab){
 	}
 
 	printf("%s %s\n", hstab->pcode, hstab->addr);
-	show(hstab->p_next);
+	show(hstab->a_next);
 }
 
-struct cell* add(struct cell *hstab, char *p, char *a) {
+struct cell* p_add(struct cell *p_hstab, char *p, char *a) {
 	struct cell *tmp;
 	tmp = malloc (sizeof(struct cell));
 
 	tmp -> pcode = p;
 	tmp -> addr = a;
-	tmp -> p_next = hstab;
+	tmp -> p_next = p_hstab;
 
 	return tmp;
 }
 
-int main() {
+struct cell* a_add(struct cell *a_hstab, char *p, char *a) {
+	struct cell *tmp;
+	tmp = malloc (sizeof(struct cell));
+
+	tmp -> pcode = p;
+	tmp -> addr = a;
+	tmp -> a_next = a_hstab;
+
+	return tmp;
+}
+
+int main(int argc , char **argv) {
 	for(int i=0; i<HSIZE; i++) {
 		p_hstab[i] = NULL;
+		a_hstab[i] = NULL;
 	}
 
 	FILE *f;
@@ -59,16 +103,49 @@ int main() {
 	while(1) {
 		char *p_buf = malloc(sizeof(char)*128);
 		char *a_buf = malloc(sizeof(char)*128);
-		
+
 		int d = fscanf(f, "%s %s", p_buf, a_buf);
 		if(d == EOF) break;
-		
+
 		int p_val = p_hash(p_buf);
-		p_hstab[p_val] = add(p_hstab[p_val], &p_buf[0], &a_buf[0]);
-		
-		printf("%d\n", p_val);
-		show(p_hstab[p_val]);
-		usleep(200000);
+		p_hstab[p_val] = p_add(p_hstab[p_val], &p_buf[0], &a_buf[0]);
+
+		int a_val = a_hash(a_buf);
+		a_hstab[a_val] = a_add(a_hstab[a_val], &p_buf[0], &a_buf[0]);
+
+		//printf("%d\n", a_val);
+		//show(a_hstab[a_val]);
+
+		//usleep(100000);
+	}
+
+	// int i, length;
+	// struct cell *p;
+	//
+	// for(i=0; i< HSIZE; i++){
+	// 		length = 0;
+	// 		p = p_hstab[i];
+	// 		while(p!=NULL){
+	// 				length++;
+	// 				p = p->p_next;
+	// 		}
+	// 		printf("%d\n\n",length);
+	// }
+
+	while(1) {
+			char *tmp = malloc(sizeof(char)*128);
+			int d = scanf("%s", tmp);
+			if(d == EOF) break;
+
+			if(sum(tmp) < 0) {
+				int a_val = a_hash(tmp);
+				a_search(a_hstab[a_val], tmp);
+			} else {
+				int p_val = p_hash(tmp);
+				p_search(p_hstab[p_val], tmp);
+			}
+
+			printf("\n");
 	}
 }
 
